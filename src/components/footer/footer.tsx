4f0e4d9df75b054abd3875b99095d42e0adb807a
ahmedsaliuGit <saliu.lito@gmail.com>
2024-10-04 10:47:00 +0100

    Clear up old folders and files

--- src/components/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => (
-  <footer className="py-6 bg-gray-800 text-center w-full">
-    <p className="text-gray-300">
-      &copy; {new Date().getFullYear()} Ahmed Saliu. All rights reserved.
-    </p>
-  </footer>
-);
-
-export default Footer;
